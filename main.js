19707d66b6cd71ac9b7d2cb3c7517018adb63f0a
Cezara Luca <cezaraluca@yahoo.com>
2020-05-31 15:49:10 +0300

    1st

--- main.js
+++ main.js
@@ -28,6 +28,26 @@ function displayResults(weather) {
     let now = new Date();
     let date = document.querySelector('.location .date');
     date.innerText = dateBuilder(now);
+
+    let temp = document.querySelector('.current .temp');
+    temp.innerHTML = `${Math.round(weather.main.temp).toFixed(0)}<span>°C</span>`
+
+    let weather_el = document.querySelector('.current .weather');
+    weather_el.innerText = weather.weather[0].main;
+
+    let hilow = document.querySelector('.hi-low');
+    hilow.innerText = `${Math.round(weather.main.temp_min)}°C / ${Main.round(weather.main.temp_max)}°C`;
 }
 
+function dateBuilder (d){
+    let months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'Octomber', 'November', 'December'];
+    let days = ['Sunday', 'Monday', 'Tuedsay', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+ 
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
 
+    return `${day} ${date} ${month} ${year}`;
+
+}
